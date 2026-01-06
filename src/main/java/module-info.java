b1582f83915e10f655cbf79a54bc31aaa7c88729
Frederik Ehmsen <Freehm01@easv365.dk>
2026-01-06 10:23:11 +0100

    Dbconnector testing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module dk.easv.mymovie {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires com.microsoft.sqlserver.jdbc;
+    requires java.sql;
 
 
     opens dk.easv.mymovie to javafx.fxml;
